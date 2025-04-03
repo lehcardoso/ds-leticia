9d3da2a85f84c062fe3ca0b9076b7104f6a385bf
Leticia Rodrigues Cardoso <167211240+lehcardoso@users.noreply.github.com>
2025-04-03 14:59:00 -0300

    laços de repetição 6, 7, 8 e 9

---
+++ Ldr9.java
@@ -0,0 +1,10 @@
+public class Ldr9 {
+    public static void main(String[] args) {
+        int numero = 101; 
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero);
+            numero++; 
+        }
+    }
+}
